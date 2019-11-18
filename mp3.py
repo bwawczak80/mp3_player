2ec7acb83b9ef0f75aa1b327d857ce9d3002070a
bwawczak80 <35830914+bwawczak80@users.noreply.github.com>
2019-11-18 09:00:17 -0600

    Add files via upload

--- mp3.py
+++ mp3.py
@@ -8,9 +8,8 @@ from io import BytesIO
 import tkinter.messagebox
 import requests as req
 
-track = 1
 main = Tk()
-main.geometry("700x700+500+20")
+main.geometry("450x550+500+20")
 frame = Frame(main)
 pygame.mixer.init()
 pygame.display.init()
@@ -18,8 +17,10 @@ pygame.display.init()
 title_label = Label(main, text="Title:").place(x=20, y=10)
 artist_label = Label(main, text="Artist:").place(x=20, y=30)
 
-display_artist = Label(main, text="Artist Placeholder").place(x=100, y=30)
-display_title = Label(main, text="Song Placeholder").place(x=100, y=10)
+display_artist = Label(main, text="")
+display_artist.place(x=100, y=30)
+display_title = Label(main, text="")
+display_title.place(x=100, y=10)
 play_list_box = Listbox(main, width=45, height=20)
 play_list_box.place(x=10, y=120)
 
@@ -33,48 +34,38 @@ def get_song_list(song_location):
         allFiles.append(fullPath)
                 
     return allFiles
-
-
-playlist = get_song_list('/home/pi/Music')
-
+directory = ('/home/pi/Music')
+playlist = get_song_list(directory)
 
 def get_details(current_song):
-    global artist_label
+    
     file_data = os.path.splitext(current_song)
-
     if file_data[1] == '.mp3':
         audio = MP3(current_song)
         artist = audio['TPE1']
         title = audio['TIT2']
-        # titleArtist['text'] = artist + " - " + title
-        try:
-            artist_label.configure(text=artist)
-            title_label.configure(text=title)
-        except:
-            tkinter.messagebox.showerror("Error", "label not configured")
-            
-        print(artist)
-        print(title)
-        
+        display_artist.config(text=artist)
+        display_title.config(text=title)
     else:
         a = pygame.mixer.Sound(current_song)
 
 
-    
-    
 def previous():
-    try:
-        global track
-        pygame.mixer.music.stop()
-        selected_song = play_list_box.curselection()
-        selected_song = int(selected_song[0] + track)
-        play_selected = playlist[selected_song]
-        pygame.mixer.music.load(play_selected)
-        pygame.mixer.music.play()
-        track -= 1
-    except:
-        tkinter.messagebox.showerror("Error", "Please select a track")
+    songs = len(playlist)
+    selected_song = play_list_box.curselection()
+    selected_song_int = int(selected_song[0])
     
+    if selected_song_int > 0:
+        pygame.mixer.music.stop()
+        next_track = play_list_box.selection_set(selected_song_int - 1,)
+        play_list_box.selection_clear(selected_song)
+        play()
+    
+    else:
+        pygame.mixer.music.stop()
+        next_track = play_list_box.selection_set(len(playlist),)
+        play_list_box.selection_clear(selected_song)
+        play()
 
 count = 1
 
@@ -88,47 +79,51 @@ def pause():
     count += 1
     
 def play():
+    
     try:
         selected_song = play_list_box.curselection()
         selected_song = int(selected_song[0])
         play_selected = playlist[selected_song]
         pygame.mixer.music.load(play_selected)
         pygame.mixer.music.play()
-        
         get_details(play_selected)
+        
     except:
         tkinter.messagebox.showerror("Error", "Please select a track")
         
-##TODO - call play() after switching play_list_box.curselection()    
+
 def next_track():
-    global track
-    try:
+    
+    songs = len(playlist)
+    selected_song = play_list_box.curselection()
+    selected_song_int = int(selected_song[0])
+    
+    if selected_song_int < songs:
         pygame.mixer.music.stop()
-        selected_song = play_list_box.curselection()
-        selected_song = int(selected_song[0] + track)
-        play_selected = playlist[selected_song]
-        pygame.mixer.music.load(play_selected)
-        pygame.mixer.music.play()
-        track += 1
-    except:
-        tkinter.messagebox.showerror("Error", "Please select a track")
+        next_track = play_list_box.selection_set(selected_song_int + 1,)
+        play_list_box.selection_clear(selected_song)
+        play()
+    
+    else:
+        pygame.mixer.music.stop()
+        next_track = play_list_box.selection_set(0,)
+        play_list_box.selection_clear(selected_song)
+        play()
+
     
 def stop():
     pygame.mixer.music.stop()
     
+    
 def load():
-    try:
-        for x in range(len(playlist)):
-            play_list_box.insert(END, playlist[x])
-            pygame.mixer.music.queue(playlist[x])
     
-        for x in range(len(playlist)):
-            playlist.append(playlist[x])
-        pygame.mixer.music.load( playlist.pop())
-    except:
-        tkinter.messagebox.showerror("Error", "No music found")
+    for x in range(len(playlist)):
+        play_list_box.insert(END, playlist[x])
+        pygame.mixer.music.queue(playlist[x])
+    
+    pygame.mixer.music.load( playlist.pop())
+
         
-    
 def subtract():
     try:
         selected_song = play_list_box.curselection()
